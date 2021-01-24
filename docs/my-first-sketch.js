f1ef67da471c5dd10d8905b72a75c22e71d4b1a6
KIlya95 <ilya.harichev@yandex.ru>
2021-01-24 15:17:53 +0300

    added

---
+++ docs/my-first-sketch.js
@@ -0,0 +1,40 @@
+// A p5.gui Template feat. Pacman
+
+// gui params
+var myAngle = 30;
+var myColor = '#eeee00';
+
+var gui;
+
+function setup() {
+
+  createCanvas(windowWidth, windowHeight);
+
+  // Create the GUI
+  sliderRange(0, 90, 1);
+  gui = createGui('p5.gui');
+  gui.addGlobals('myColor', 'myAngle');
+  
+  gui2 = createGui('Buttin');
+  gui2.addButton('Button');
+  
+  // Only call draw when then gui is changed
+  noLoop();  
+}
+
+
+function draw() {
+    
+  // this is a piece of cake
+  background(0);
+  fill(myColor);
+  angleMode(DEGREES);
+  arc(width/2, height/2, 100, 100, myAngle/2, 360 - myAngle/2, PIE);
+  
+}
+
+
+// dynamically adjust the canvas to the window
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
